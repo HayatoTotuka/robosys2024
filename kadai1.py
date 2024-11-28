46fe6d78c6befbc10cf0d36fa801ebe9f12e95e4
Hayato Totuka <s23c1094nz@s.chibakoudai.jp>
2024-11-28 20:05:09 +0900

    課題

---
+++ kadai1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+#SPDX-FileCopyrightText: 2024 totuka hayato
+# SPDX-License-Identifier: BSD-3-Clause
+
+import re
+import sys
+
+text = sys.stdin.read().strip()
+
+new_text = re.sub(r"[^\wぁ-んァ-ン一-龯]", "", text)
+
+japanese_pattern = r"[ぁ-んァ-ン一-龯]"
+english_pattern = r"[a-zA-Z]"
+number_pattern = r"[0-9]"
+
+japanese_text = re.findall(japanese_pattern, text)
+english_text = re.findall(english_pattern, text)
+number_text = re.findall(number_pattern, text)
+
+length = len(new_text)
+length_japanese = len(japanese_text)
+length_english = len(english_text)
+length_number = len(number_text)
+
+print("入力された文字数は:",length)
+print("入力された日本語の数は: ",length_japanese)
+print("入力された英語の数は: ",length_english)
+print("入力された数字の数は: ",length_number)
--- test.bash
+++ test.bash
@@ -6,12 +6,86 @@ ng () {
 	res=1
 }
 
-
 res=0
 
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
-[ "${res}" = 0 ] && echo OK
+
+
+out=$(echo 123おはようhello | ./kadai1.py)
+[[ "$out" == *"入力された文字数は: 12"* ]]
+out=$(echo 123おはようhello | ./kadai1.py)
+[[ "$out" == *"入力された日本語の数は: 4"* ]]
+out=$(echo 123おはようhello | ./kadai1.py)
+[[ "$out" == *"入力された英語の数は: 5"* ]]
+out=$(echo 123おはようhello | ./kadai1.py)
+[[ "$out" == *"入力された数字の数は: 3"* ]]
+
+out=$(echo | ./kadai1.py)
+[[ "$out" == *"入力された文字数は: 0"* ]]
+out=$(echo | ./kadai1.py)
+[[ "$out" == *"入力された日本語の数は: 0"* ]]
+out=$(echo | ./kadai1.py)
+[[ "$out" == *"入力された英語の数は: 0"* ]]
+out=$(echo | ./kadai1.py)
+[[ "$out" == *"入力された数字の数は: 0"* ]]
+
+out=$(echo おはようhello | ./kadai1.py)
+[[ "$out" == *"入力された文字数は: 9"* ]]
+out=$(echo おはようhello | ./kadai1.py)
+[[ "$out" == *"入力された日本語の数は: 4"* ]]
+out=$(echo おはようhello | ./kadai1.py)
+[[ "$out" == *"入力された英語の数は: 5"* ]]
+out=$(echo おはようhello | ./kadai1.py)
+[[ "$out" == *"入力された数字の数は: 0"* ]]
+
+out=$(echo 123hello | ./kadai1.py)
+[[ "$out" == *"入力された文字数は: 8"* ]]
+out=$(echo 123hello | ./kadai1.py)
+[[ "$out" == *"入力された日本語の数は: 0"* ]]
+out=$(echo 123hello | ./kadai1.py)
+[[ "$out" == *"入力された英語の数は: 5"* ]]
+out=$(echo 123hello | ./kadai1.py)
+[[ "$out" == *"入力された数字の数は: 3"* ]]
+
+out=$(echo 123おはよう | ./kadai1.py)
+[[ "$out" == *"入力された文字数は: 7"* ]]
+out=$(echo 123おはよう | ./kadai1.py)
+[[ "$out" == *"入力された日本語の数は: 4"* ]]
+out=$(echo 123おはよう | ./kadai1.py)
+[[ "$out" == *"入力された英語の数は: 0"* ]]
+out=$(echo 123おはよう | ./kadai1.py)
+[[ "$out" == *"入力された数字の数は: 3"* ]]
+
+out=$(echo 123 | ./kadai1.py)
+[[ "$out" == *"入力された文字数は: 3"* ]]
+out=$(echo 123 | ./kadai1.py)
+[[ "$out" == *"入力された日本語の数は: 0"* ]]
+out=$(echo 123 | ./kadai1.py)
+[[ "$out" == *"入力された英語の数は: 0"* ]]
+out=$(echo 123 | ./kadai1.py)
+[[ "$out" == *"入力された数字の数は: 3"* ]]
+
+out=$(echo おはよう | ./kadai1.py)
+[[ "$out" == *"入力された文字数は: 4"* ]]
+out=$(echo おはよう | ./kadai1.py)
+[[ "$out" == *"入力された日本語の数は: 4"* ]]
+out=$(echo おはよう | ./kadai1.py)
+[[ "$out" == *"入力された英語の数は: "* ]]
+out=$(echo おはよう | ./kadai1.py)
+[[ "$out" == *"入力された数字の数は: "* ]]
+
+out=$(echo hello | ./kadai1.py)
+[[ "$out" == *"入力された文字数は: 5"* ]]
+out=$(echo hello | ./kadai1.py)
+[[ "$out" == *"入力された日本語の数は: 0"* ]]
+out=$(echo hello | ./kadai1.py)
+[[ "$out" == *"入力された英語の数は: 5"* ]]
+out=$(echo hello | ./kadai1.py)
+[[ "$out" == *"入力された数字の数は: 0"* ]]
+
+
+[ "${res}" = 0 ] && echo OK 
 
 exit $res
